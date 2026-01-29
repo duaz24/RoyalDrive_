816796a2daeb3d49aaad63c6294a8cd0e5ce11c9
Duarte Azevedo <A047329@ipmaia.pt>
2026-01-29 11:16:21 +0000

    Update chatbotRoutes.js

--- RoyalDrive-main/routes/chatbotRoutes.js
+++ RoyalDrive-main/routes/chatbotRoutes.js
@@ -2,7 +2,5 @@
 const express = require('express');
 const router = express.Router();
 const chatbotController = require('../controllers/chatbotController');
-
 router.post('/message', chatbotController.processMessage);
-
 module.exports = router;
