46276998d88507cea2f72210a7bfa27a75bcaade
Duarte Azevedo <A047329@ipmaia.pt>
2026-01-29 20:42:44 +0000

    Update authRoutes.js

--- RoyalDrive-main/routes/authRoutes.js
+++ RoyalDrive-main/routes/authRoutes.js
@@ -3,16 +3,10 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 const authMiddleware = require('../middleware/authMiddleware');
 
-// Registar conta normal
-router.post('/register', authController.register);
-
-// Login normal
-router.post('/login', authController.login);
-
-// Login com Google
+// --- Apenas Login com Google ---
 router.post('/google', authController.googleLogin);
 
-// Obter dados do user logado
+// --- Obter dados do user logado ---
 router.get('/me', authMiddleware, authController.getMe);
 
 module.exports = router;
