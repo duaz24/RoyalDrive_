e37312b626b24eb095679c37d09e24e269854f42
Duarte Azevedo <A047329@ipmaia.pt>
2026-01-29 10:44:50 +0000

    Create chatbotRoutes.js

---
+++ RoyalDrive-main/routes/chatbotRoutes.js
@@ -0,0 +1,8 @@
+// routes/chatbotRoutes.js
+const express = require('express');
+const router = express.Router();
+const chatbotController = require('../controllers/chatbotController');
+
+router.post('/message', chatbotController.processMessage);
+
+module.exports = router;
